0b1fa67e477e844262f1c64db8235292378eabeb
izzy2lost <izzynochill@gmail.com>
2025-02-18 05:52:25 -0500

    Update Game.cs

--- src/openrct2-unity/Assets/Scripts/Bindings/Game.cs
+++ src/openrct2-unity/Assets/Scripts/Bindings/Game.cs
@@ -7,22 +7,18 @@ namespace OpenRCT2.Bindings
 {
     public class Game
     {
-        // Configuration for data paths
-        readonly string _openRCT2DataPath;
-        readonly string _rct2Path;
-        readonly string? _rct1Path;
-
+        // Hardcoded paths for Xbox UWP Dev Mode
+        readonly string _openRCT2DataPath = "E:/OpenRCT2/data";
+        readonly string _rct2Path = "E:/Games/Rollercoaster Tycoon 2";
+        readonly string? _rct1Path = "E:/Games/Rollercoaster Tycoon 1";
 
         /// <summary>
         /// Starts the game based on the settings set in the editor.
         /// </summary>
-        public Game(string openRCT2DataPath, string rct2Path, string? rct1Path)
+        public Game()
         {
-            _openRCT2DataPath = openRCT2DataPath;
-            _rct2Path = rct2Path;
-            _rct1Path = rct1Path;
+            Debug.Log("Xbox UWP Dev Mode: Using hardcoded paths on E:/ drive.");
         }
-
 
         /// <summary>
         /// Starts the game by opening a park.
@@ -38,7 +34,6 @@ namespace OpenRCT2.Bindings
             Debug.Log($"OpenRCT2 started on park: '{parkname}' (startup time: {watch.Elapsed})");
         }
 
-
         /// <summary>
         /// Shuts down the game.
         /// </summary>
@@ -46,7 +41,6 @@ namespace OpenRCT2.Bindings
         {
             StopGame();
         }
-
 
         /// <summary>
         /// Performs a single game update.
@@ -56,22 +50,14 @@ namespace OpenRCT2.Bindings
             PerformGameUpdate();
         }
 
-
         /// <summary>
-        /// Starts the game with the specified folder paths paths.
+        /// Starts the game with the specified folder paths.
         /// </summary>
-        /// <param name="datapath">
-        /// The data folder of OpenRCT2.
-        /// </param>
-        /// <param name="rct2path">
-        /// The absolute path to the RCT2 directory.
-        /// </param>
-        /// <param name="rct1path">
-        /// The absolute path to the RCT1 directory (optional).
-        /// </param>
+        /// <param name="datapath">The data folder of OpenRCT2.</param>
+        /// <param name="rct2path">The absolute path to the RCT2 directory.</param>
+        /// <param name="rct1path">The absolute path to the RCT1 directory (optional).</param>
         [DllImport(Plugin.FileName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         static extern void StartGame([MarshalAs(UnmanagedType.LPStr)] string datapath, [MarshalAs(UnmanagedType.LPStr)] string rct2path, [MarshalAs(UnmanagedType.LPStr)] string? rct1path = default);
-
 
         /// <summary>
         /// Performs a single game update.
@@ -79,13 +65,11 @@ namespace OpenRCT2.Bindings
         [DllImport(Plugin.FileName, CallingConvention = CallingConvention.Cdecl)]
         static extern void PerformGameUpdate();
 
-
         /// <summary>
         /// Shuts down the game.
         /// </summary>
         [DllImport(Plugin.FileName, CallingConvention = CallingConvention.Cdecl)]
         static extern void StopGame();
-
 
         /// <summary>
         /// Loads a park from the specified path.
